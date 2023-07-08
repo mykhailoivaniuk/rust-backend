ea5454e391d383fd965011d91a867977a2c53c9b
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2023-07-08 17:28:04 -0400

    add CI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let _idx: u8 = 2;
+    let tup: (f32, char, f64, &str) = (10.5, 'c', 4.5, "less");
+    // prefixing with _ lets clippy know that its intentionally unused
+    let mut some_name_here: [i32; 4] = [0; 4];
+    some_name_here[0] = 1;
+    ls();
+    println!("{}", tup.0);
+}
+
+fn ls(){
+    println!("Ls");
 }
